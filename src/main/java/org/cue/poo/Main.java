efbbf27918469c329410032f504ad63338c80cb9
Carlos Aranzazu <caaranzazu_230@cue.edu.co>
2024-11-11 22:59:45 -0500

    Creamos un proyecto para resolver una ecuacion diferencial de segundo orden homogenea

--- src/main/java/org/cue/poo/Main.java
+++ src/main/java/org/cue/poo/Main.java
@@ -1,11 +1,26 @@
 package org.cue.poo;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
 //        UIMenu.showMenu();
-        Patient paciente = new Patient();
-        paciente.setName("Carlos Augusto");
-        System.out.println(paciente.getName());
+        Patient paciente1 = new Patient("Carlos Augusto","carlos@correo.com");
+//        paciente.setName("Carlos Augusto");
+//        System.out.println(paciente.getName());
+        ArrayList<Patient> patients = new ArrayList<>();
+        System.out.println(patients);
+        System.out.println("*****************");
+        patients.add(paciente1);
+        System.out.println(patients);
+        System.out.println("*****************");
+
+
+
+
+
+
+
     }
 }
